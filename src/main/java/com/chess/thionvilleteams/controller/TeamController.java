9fb97e4ac6a51d67a2ad8075f2a787b7a429fc66
Melvin DOUCET <doucet.m@sfeir.com>
2023-01-26 21:21:53 +0100

    rename service property

--- src/main/java/com/chess/thionvilleteams/controller/TeamController.java
+++ src/main/java/com/chess/thionvilleteams/controller/TeamController.java
@@ -12,24 +12,24 @@ import java.util.List;
 @RequestMapping("/api/v1/teams")
 @SuppressWarnings("unused")
 public class TeamController {
-    private final TeamService teamService;
+    private final TeamService service;
 
     public TeamController(TeamService teamService) {
-        this.teamService = teamService;
+        this.service = teamService;
     }
 
     @GetMapping
     public ResponseEntity<Team> getOne(@RequestParam("id") long id) {
-        return new ResponseEntity<>(teamService.getById(id), HttpStatus.OK);
+        return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public ResponseEntity<List<Team>> getAll() {
-        return new ResponseEntity<>(teamService.getAll(), HttpStatus.OK);
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<Team> createOne(@RequestBody Team team) {
-        return new ResponseEntity<>(teamService.create(team), HttpStatus.OK);
+        return new ResponseEntity<>(service.create(team), HttpStatus.OK);
     }
 }
